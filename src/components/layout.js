cb01d32ee4bba874314f514b5c9b4d9d43271a20
Andreas Nauleau <anauleau@gmail.com>
2020-04-25 08:17:31 -0700

    Adds gatsby starter, updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,9 +35,9 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          {new Date().getFullYear()}, Built by
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://www.github.com/anauleau">anaualeau</a>
         </footer>
       </div>
     </>
